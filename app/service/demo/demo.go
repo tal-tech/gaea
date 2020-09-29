c5ff7f9ea48b88168801e64a03be066aee8ca96e
wanglongfei <wanglongfei@100tal.com>
2020-09-29 12:55:55 +0800

    demo DoFun返回成功

--- app/service/demo/demo.go
+++ app/service/demo/demo.go
@@ -2,19 +2,11 @@ package demo
 
 import (
 	"context"
-	"math/rand"
-
-	logger "github.com/tal-tech/loggerX"
 )
 
 func DoFun(ctx context.Context, param string) (ret map[string]interface{}, err error) {
-	if rand.Intn(100)%2 == 0 {
-		return nil, logger.NewError("dofun fail")
-	} else {
-		ret = make(map[string]interface{}, 2)
-		ret["ret1"] = "dofun ok"
-		ret["ret1"] = "Welcome to use gaea!"
-
-	}
+	ret = make(map[string]interface{}, 2)
+	ret["ret1"] = "dofun ok"
+	ret["ret1"] = "Welcome to use gaea!"
 	return
 }
